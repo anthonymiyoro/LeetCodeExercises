7916a46dd0371386697bb41bb59c038786d0fa40
anthonymiyoro <amiyoro@gmail.com>
2016-06-02 07:59:36 +0300

    Added new project

---
+++ src/age.java
@@ -0,0 +1,17 @@
+public class age {
+  public static void main (String[] args){
+	  
+	 String firstName,middleName,lastName;
+	 char firstInitial,middleInitial,lastInitial;
+	 
+	 firstName= "Anthony";
+	 middleName= "Onteri";
+	 lastName= "Miyoro";
+	 
+	 firstInitial = 'A';
+	 middleInitial= 'O';
+	 lastInitial= 'm';	 
+	 
+	 System.out.println )("My name is" + " " + firstName + " " + middleName + " " + lastName);
+  }
+}
