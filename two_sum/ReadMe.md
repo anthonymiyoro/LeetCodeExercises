b52d2337b484566ab0b40951c29304c4cb230bcf
Anthony Miyoro <amiyoro@gmail.com>
2019-04-30 12:09:33 +0300

    Updated Readme

--- two_sum/ReadMe.md
+++ two_sum/ReadMe.md
@@ -1,3 +1,8 @@
+## TWO SUM
+[Found Here!!!](https://leetcode.com/problems/two-sum/)
+
+### QUESTION
+
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -10,4 +15,12 @@ You may assume that each input would have exactly one solution, and you may not 
     Because nums[0] + nums[1] = 2 + 7 = 9,
     return [0, 1].
     
+'''
+
+### SOLUTION
+
+'''
+
+    My proposed soltion uses a nested for loop to loop through 2 copies of a list and add each other eg [2, 7, 11, 15] + [2, 7, 11, 15] one by one for each item. This is a "brute force" solution. And can be improved.
+
 '''
--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -1,18 +1,19 @@
-nums_list = [2, 7, 11, 15]
+nums_list = [4, 6, 8, 2, 7, 11, 15]
 target = 9
 
-def twoSum(nums, target):
-        answer_list = []
-        for i in nums_list:
-                for j in nums_list:
-                        if (i+j) == target:
-                                answer_list.append(nums_list.index(i))
-                                answer_list.append(nums_list.index(j))
-                                print (answer_list)
-                                return(answer_list)
-                        else:
-                                print ("Nope")
+class Solution:
+        def twoSum(self, nums, target):
+                answer_list = []
+                for i in nums_list:
+                        for j in nums_list:
+                                if (i+j) == target:
+                                        answer_list.append(nums_list.index(i))
+                                        answer_list.append(nums_list.index(j))
+                                        print (answer_list)
+                                        return(answer_list)
+                                else:
+                                        print ("Nope")
 
-
-twoSum(nums_list, target)
+solution = Solution()
+solution.twoSum(nums_list, target)
    
