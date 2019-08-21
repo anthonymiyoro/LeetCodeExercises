ce6eeb579f2d37cdca87acb5b819a1bef13b09d4
Anthony Miyoro <amiyoro@gmail.com>
2019-08-21 13:19:34 +0300

    Final answer

--- substring_problem/substring.py
+++ substring_problem/substring.py
@@ -1,45 +1,20 @@
-A = "cdabcdab"
-B = "abcd"
-counter = 0
-appended_string = ""
-string_length = len(B)
-print ("String Length", string_length)
-
-
-char_index = 0
-while char_index <= len(A):
-    # print(A[char_index])
-    
-    # Build A to new string using each character one by one
-    appended_string = appended_string + (A[char_index])
-    
-
-    # search using last 2 characters of string B
-    search_string_end = B[-2:]
-    search_string_start = B[:2]
-    
-    if appended_string.endswith(search_string_end):
-        print ("appended_string_end", appended_string)
-        # Reset result string
-        appended_string = ""
-        counter = counter + 1
-        print (counter)
-        print (" ")
-        char_index += 1
-
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        # initialise variables
+        temp = ""
+        counter = 0
         
-    elif appended_string.startswith(search_string_start):
-        print ("appended_string_start", appended_string)
-        # Reset result string
-        appended_string = ""
-        counter = counter + 1
-        print (counter)
-        print (" ")
-        # if we find a match at the begonning of the string, skip to next part of string
-        char_index = char_index + (2-string_length)
-        
-    else:
-        print ("appended_string", appended_string)
-        print (" ")
-        char_index += 1
-        
+        # Append to temporary string and check for substring while smaller than original size
+        while len(temp) < len(B):
+            temp = temp + A
+            counter = counter + 1
+            # Check if substring exists
+            if B in temp:
+                return counter
+            
+        # if still no match in larger than B appended string, continue appending
+        temp = temp + A
+        # Check if substring exists
+        if B in temp:
+            return counter + 1
+        return -1
