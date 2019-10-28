1d2e4e8c58289bacd6f39fc76daeb3b7f32a0d88
Anthony Miyoro <amiyoro@gmail.com>
2019-10-28 21:06:11 +0300

    added kdistinct part solution

---
+++ count_substrings_with_k_distinct_characters/k_distinct.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraysWithKDistinct(self, s, k):
+        if K > len(s):
+            return 0
+        res = 0
+        
+        # Looping through string
+        for i in range(len(s)):
+            # Convert string to array of distinct values
+            dist = set()
+            for j in range(i, len(s))
+            
+            
