eee2142626c40f6b40cd4df90068aaef7e9a577f
Anthony Miyoro <amiyoro@gmail.com>
2019-12-04 20:03:21 +0300

    Working understood solution

--- next_highest_tempereture-min_discount/next_highest.py
+++ next_highest_tempereture-min_discount/next_highest.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 class Solution:
-    def dailyTemperatures(self, T: List[int]) -> List[int]:
-        # create a new list that will hold the solution and one that contains question items
+    def dailyTemperatures(self, T):
+        # create a new list that will hold the solution and 
+        # one that contains question items
         hotter = []
         temperature_list = np.array(T)
         
-        # loop through list with question variables and compare currently se;ected
-        # item with next item in list
+        # loop through list with question variables and compare currently 
+        # selected item with next item in list 
         for index, temperature in enumerate(temperature_list):
             hotter.append(np.argmax(temperature_list[index:] > temperature))
-            print ("hotter", hotter)
             
+            # print ("hotter", hotter)
+        print (hotter)
         return hotter
-        
+
+T = [73, 74, 75, 71, 69, 72, 76, 73]
+solution = Solution()
+solution.dailyTemperatures(T)
