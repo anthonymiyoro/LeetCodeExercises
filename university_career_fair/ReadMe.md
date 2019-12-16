42369aec614e42f5e91d02e01e269c2999e6199a
Anthony Miyoro <amiyoro@gmail.com>
2019-12-16 16:32:07 +0300

    Understood and explained caeer fair problem

--- university_career_fair/ReadMe.md
+++ university_career_fair/ReadMe.md
@@ -8,3 +8,25 @@
 
 ### Proposed Solution
 Make use of the Interval Scheduling Algorithm as found [here](https://www.codespeedy.com/interval-scheduling-in-python/)
+
+- Loop through arrival time array
+- If a new event arrives during or after another event has ended add its arrival time to max set and set the previous event time to the finish time for this event
+- Return the maximum of the max_set
+
+```
+INPUT:
+universityCareerFair([1, 3, 3, 5, 7], [2, 2, 1, 2, 1])
+
+
+--------------------------------- ---------------------------------
+OUTPUT:
+('finish_times', [3, 5, 4, 7, 8])
+('max set', set([0]))
+('max set', set([0, 1]))
+('max set', set([0, 1]))
+('max set', set([0, 1, 3]))
+('max set', set([0, 1, 3, 4]))
+('Maximum number of tasks can be executed are', 4)
+
+```
+
--- university_career_fair/university_career_fair.py
+++ university_career_fair/university_career_fair.py
@@ -5,17 +5,26 @@
 # print(universityCareerFair([1, 2], [7, 3])) # 1
 
 def universityCareerFair(arrival, duration):
+    # add duration to arrival time to get finish time for each
     finish = list(map(sum, zip(duration,arrival)))
+    # get number of events
     index = list(range(len(arrival)))
-    print ("index", index)
     
+    print ('finish_times', finish)
+    
+    # dictionary to hold max 
     max_set = set()
     prev_event_time = 0
     for i in index:
+        # if arrival time is greater or equal to previous event 
+        # start time add to max_set
         if arrival[i] >= prev_event_time:
             max_set.add(i)
+        # set the previous event time to the finish time of current event
             prev_event_time = finish[i]
-            
+        
+        print ('max set',max_set)
+        #   get the largest number from the max set  
     return max(max_set)
 
 result = universityCareerFair([1, 3, 3, 5, 7], [2, 2, 1, 2, 1])
