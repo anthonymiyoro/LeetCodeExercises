4d48500b17b2c0234add18fd478a1fc430f1a01c
Anthony Miyoro <amiyoro@gmail.com>
2019-12-17 15:59:57 +0300

    Kind of understod min discount

--- min_discount/min_discount.py
+++ min_discount/min_discount.py
@@ -1,43 +1,33 @@
+# This function returns the final prices of items after discount
 class Solution:
     def finalPrice(self, prices):
+        result_list = []
         if len(prices) == 0:
             return 0
         
         # Store the Next Lesser Element
         nle_stack = []
+        # Initialise result
         res = 0
         
-        # Move from right to left
+        # Move from right to left appending prices to nle_stack
         for i in reversed(range(len(prices))):
-            # pop the stack until we find the next lesser element
+            # If the sum of nle_stack items is greater than the corresponding price pop right most nle_stack item
             while nle_stack and nle_stack[-1] > prices[i]:
                 nle_stack.pop()
-            
+
             # add the discount, if any, and then add the current price to the stack
             res = res + prices[i] if not nle_stack else prices[i] - nle_stack[-1]
-            nle_stack.append(prices[i])
+            # append discounted prices to list then reverse it back
+            result_list.append(res)
+            result_list.reverse()
             
-        print ("nle_stack", nle_stack)
+            nle_stack.append(prices[i])
+        
+        
+        print (result_list)
         return res
     
-    # def finalPrice(self, prices):
-    #     stack = []
-    #     res = 0
-        
-    #     for i in prices:
-    #         if(stack):
-    #             if i > stack[-1]:
-    #                 stack.append(i)
-    #             else:
-    #                 while(stack and i <= stack[-1]):
-    #                     res += stack[-1] - i
-    #                     stack.pop(-1)
-    #                 stack.append(i)
-    #         else:
-    #             stack.append(i) 
-    #     print (stack)
-    #     print (res + sum(stack) )        
-    #     return res + sum(stack) 
     
     
 prices = [5, 4, 5, 1, 3, 3, 8, 2]
