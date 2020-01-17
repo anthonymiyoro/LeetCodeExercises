0c357f5b1a43e45d4a9215914c107b1dac262f23
Anthony Miyoro <amiyoro@gmail.com>
2020-01-17 16:42:54 +0300

    Added working solution

--- add_two_numbers_as_linked_list/solution.py
+++ add_two_numbers_as_linked_list/solution.py
@@ -1,27 +1,47 @@
 # Definition for singly-linked list.
-class ListNode(object):
-  def __init__(self, x):
-    self.val = x
-    self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
-  def addTwoNumbers(self, l1, l2, c = 0):
-    # Fill this in.
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        len1, len2 = self.getLength(l1), self.getLength(l2)
+        l1 = self.addLeadingZeros(len2-len1, l1)
+        l2 = self.addLeadingZeros(len1-len2, l2)
+        c, ans = self.combineList(l1, l2)
+        if c>0:
+            l3 = ListNode(c)
+            l3.next = ans
+            ans = l3
+        return ans
 
-    l1 = ListNode(2)
-    l1.next = ListNode(4)
-    l1.next.next = ListNode(3)
-    print (l1)
+    # get lenght of a linked list
+    def getLength(self, node):
+        l = 0
+        while node:
+            l += 1
+            node = node.next
+        return l
 
-    l2 = ListNode(5)
-    l2.next = ListNode(6)
-    l2.next.next = ListNode(4)
-    print (l2)
+    # add the leading 0s to the linked list
+    def addLeadingZeros(self, n, node):
+        for i in range(n):
+            new = ListNode(0)
+            new.next = node
+            node = new
+        return node
 
-    result = Solution().addTwoNumbers(l1, l2)
-    while result:
-        print (result.val),
-        result = result.next
+    
+    def combineList(self, l1, l2):
+        if (not l1 and not l2):
+            return (0, None)
+        c, new = self.combineList(l1.next, l2.next)
+        s = l1.val+l2.val+c
+        ans = ListNode(s % 10)
+        ans.next = new
+        return (s/10, ans)
+        
 
 mySolution = Solution()
 mySolution.addTwoNumbers([2,4,3], [5,6,4], c = 0)
