c9560e31e17dbc974cc8d115597049c60b0399e8
Anthony Miyoro <amiyoro@gmail.com>
2020-01-20 16:28:16 +0300

    Added copy list problem

---
+++ copy_list_with_random_pointer/copy_list.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        
+        # Create new empty node for each nodes
+        node = head
+        while node:
+            node.random = Node(node.val, node.random, None)
+            node = node.next
+            
+        # Populate random field of each of the new nodes
+        node = head
+        while node:
+            node.random.random = node.random.next.random if node.random.next else None
+            node = node.next
+        
+        # Restore original list and build new list
+        head_copy = head.random if head else None
+        node = head
+        while node:
+            node.random.next = node.next.random if node.next else None
+            node.random = node.random.next
+            node = node.next
+            
+        return head_copy
